5b5826d9f4334f006815e4fcee39b4135e0633c8
zubeir abib <zubeirabib@gmail.com>
2025-07-20 10:51:55 +0300

    correcting view imports

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('library_detail/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('list_books/', views.list_books, name='book_list'),
+    path('library_detail/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('list_books/', list_books, name='book_list'),
 ]
