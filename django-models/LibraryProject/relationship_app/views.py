5664e838ac700a5e2d01fe9161969911ece04817
zubeir abib <zubeirabib@gmail.com>
2025-07-20 10:22:14 +0300

    improving folder structure

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,16 +5,19 @@ from .models import Book, Library
 def book_list(request):
       books = Book.objects.all()
       context = {'book_list': books}
-      return render(request, 'list_books.html', context)
+      return render(request, 'relationship_app/list_books.html', context)
 
 class LibraryDetailView(DetailView):
-  model = Library
-  template_name = 'library_detail.html'
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
 
-  def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
-    library = self.get_object()
-    context['library details'] = library.all()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        library = self.get_object()
+        context['library_books'] = library.books.all()
+        context['librarian'] = getattr(library, 'librarian', None)
+        return context
+
 
 
 
