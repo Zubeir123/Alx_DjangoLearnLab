bb3b6f39b138283a9417d315c69ce9f351e4a409
zubeir abib <zubeirabib@gmail.com>
2025-07-21 00:07:53 +0300

    satisfying the checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,8 +6,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
-
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import permission_required
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
