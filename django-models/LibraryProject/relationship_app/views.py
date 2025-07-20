c605c277b16e183c020b3f3e05f6f8b4b5a30062
zubeir abib <zubeirabib@gmail.com>
2025-07-20 10:32:46 +0300

    clarifying library import for the checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
+from .models import Book
+from .models import Library
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
 
 def book_list(request):
       books = Book.objects.all()
