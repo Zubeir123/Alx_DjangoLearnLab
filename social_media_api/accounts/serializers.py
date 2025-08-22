376b41fa0271e8ef62813d87478a3a3a1789ec85
zubeir abib <zubeirabib@gmail.com>
2025-08-22 09:32:23 +0300

    serializer optimizing

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,26 +1,32 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.password_validation import validate_password
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password]
+    )
 
-class RegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['username', 'email', 'password', 'password2']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Passwords didn't match."})
+        return attrs
 
     def create(self, validated_data):
-        user = User.objects.create(
+        validated_data.pop('password2')
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=make_password(validated_data['password'])
+            password=validated_data['password']
         )
-        Token.objects.create(user=user)
         return user
