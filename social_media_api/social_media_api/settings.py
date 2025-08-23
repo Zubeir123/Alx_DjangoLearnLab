b5fc197d390203819f09c2af83e04a0b543ab8b2
zubeir abib <zubeirabib@gmail.com>
2025-08-23 10:32:38 +0300

    Fix: database config with PORT for Heroku

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -83,10 +83,9 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
